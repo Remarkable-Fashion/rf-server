ab02ffb7191e01e1bf151b9c2bcce4945d9e694b
DoHan Kim <50390565+ehgks0000@users.noreply.github.com>
2022-11-19 00:36:36 +0900

    Feature/prisma (#1)
    * Feat: Add docker mysql container
    
    1. mysql user 계정 권한 부여 sql 추가
    2. Readme 수정
    
    * Feat: Add docker mysql container
    
    1. mysql user 계정 권한 부여 sql 추가
    2. Readme 수정
    
    * Feat : Add seed.ts 추가
    
    * Feat: Add dbErrorMiddleware 추가
    
    1. dbErrorMiddleware 추가 및 적용
    2. `/api/v1/db-test` route 추가

--- README.md
+++ README.md
@@ -6,6 +6,12 @@
 $ docker-compose up --build -d
 ```
 
+1. 도커 실행 후 mysql 테이블 생성을 위해 `npm run db:push` 입력.
+
+> `.env`의 `DATABASE_URL`를 localhost로 사용해야함.
+
+2. seed 실행 `npm run seed`
+
 ### 접속 방법
 
 1. localhost:3000 // http 사용
--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,5 +1,17 @@
 import type { Request, Response, NextFunction } from "express";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { logger } from "../logger";
 
-export const errorMiddleware = (_err: unknown, _req: Request, res: Response, _next: NextFunction) => {
+export const dbErrorMiddleware = (err: unknown, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof PrismaClientKnownRequestError) {
+        logger.error({ req, res });
+        res.status(400).json({ msg: err.message });
+    } else {
+        next();
+    }
+};
+
+export const errorMiddleware = (_err: unknown, req: Request, res: Response, _next: NextFunction) => {
+    logger.error({ req, res });
     res.status(500).json({ msg: "error test" });
 };
