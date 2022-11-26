639f010147f853d73d6562e79f9e8131e35e7992
DoHan Kim <50390565+ehgks0000@users.noreply.github.com>
2022-11-26 13:04:41 +0900

    Feature/post (#5)
    * Feat: Add db schema
    
    1. Posts, Images, Clothes schema 추가
    2. 각 테이블의 id, UnsignedInt로 수정
    3. post migration.sql 추가
    
    * Feat: Add create Post
    
    1. create-post 컨트롤러, 서비스, 라우터 추가
    2. errorMiddleware 수정
    3. seed.ts 수정
    4. migration.sql 수정

---
+++ src/domains/posts/service/create-post.ts
@@ -0,0 +1,34 @@
+import { type PrismaClient } from "@prisma/client";
+import { Clothes } from "../types";
+
+export const createPost = (
+    { userId, title, description, imgUrls, clothes }: { userId: number; title: string; description: string; imgUrls: string[]; clothes?: Clothes[] },
+    prisma: PrismaClient
+) => {
+    const isClosthes = clothes && clothes.length > 0;
+    return prisma.posts.create({
+        select: {
+            userId: true,
+            title: true,
+            description: true,
+            images: true,
+            clothes: true
+            // ...(isClosthes && { clothes: true })
+        },
+        data: {
+            userId,
+            title,
+            description,
+            images: {
+                create: imgUrls.map((url) => ({ url }))
+            },
+            ...(isClosthes
+                ? {
+                      clothes: {
+                          create: clothes.map((e) => ({ ...e }))
+                      }
+                  }
+                : {})
+        }
+    });
+};
