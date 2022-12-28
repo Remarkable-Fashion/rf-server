90c3e1811dff4a8e33204e760ade553cc5592fad
DoHan Kim <50390565+ehgks0000@users.noreply.github.com>
2022-12-28 23:11:58 +0900

    Feature: Add api rate-limit (#12)
    1. 15분에 최대 100번의 req가 가능하게 제한.
    2. redis에 ip 기준으로 저장.

---
+++ src/middleware/api-rate-limit.ts
@@ -0,0 +1,17 @@
+import rateLimit from "express-rate-limit";
+import RedisStore from "rate-limit-redis";
+import { conf } from "../config";
+import { getRedis } from "../db/redis";
+
+export const apiLimiter = rateLimit({
+    windowMs: conf().RATELIMIT_WINDOW,
+    max: conf().RATELIMIT_MAX, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    store: new RedisStore({
+        sendCommand: (...args: string[]) => getRedis().sendCommand(args)
+    }),
+    message: async () => {
+        return "You can only make 100 request per 15 miniutes";
+    }
+});
