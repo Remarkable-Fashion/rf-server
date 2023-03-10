b0590bebbbfff95a811ca5ed1f15006a9c6ec602
DoHan Kim <50390565+ehgks0000@users.noreply.github.com>
2023-03-10 11:32:50 +0900

    Chore: WithoutNull -> ExcludeNullAndPartial (#19)
    1. 기존 WithoutNull 타입을 ExcludeNull과 ExcludeNullAndPartial 으로 세분화했습니다.

--- src/domains/users/service/update-user.ts
+++ src/domains/users/service/update-user.ts
@@ -1,7 +1,7 @@
 import type { PrismaClient, Profile } from "@prisma/client";
-import { WithoutNull } from "../../../lib/types";
+import { ExcludeNullAndPartial } from "../../../lib/types";
 
-export type UpdateProfile = WithoutNull<Pick<Profile, "sex" | "height" | "weight" | "introduction">>;
+export type UpdateProfile = ExcludeNullAndPartial<Pick<Profile, "sex" | "height" | "weight" | "introduction">>;
 
 export const updateUser = async (userId: number, data: UpdateProfile, prisma: PrismaClient) => {
     return prisma.profile.update({where: { userId }, data });
