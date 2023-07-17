b040bcf22a1bb50646bdff141cf5bd52b3a4e4ff
ehgks0000 <ehgks0083@gmail.com>
2023-07-17 11:59:58 +0900

    ADD: 엘라스틱서치 seed 추가

---
+++ src/db/seeds/elasticsearch/index.ts
@@ -0,0 +1,12 @@
+import { Client } from "@elastic/elasticsearch";
+import { createClothesIndex, createPostIndex, createSearchLogIndex } from "./scripts/create-index";
+import { createIdWithTimestampPipeline, createTimestampPipeline } from "./scripts/create-piptline";
+export const seedElasticsearch = async (client: Client) => {
+
+    await createTimestampPipeline(client);
+    await createIdWithTimestampPipeline(client);
+    
+    await createPostIndex(client);
+    await createClothesIndex(client);
+    await createSearchLogIndex(client);
+}
---
+++ src/db/seeds/elasticsearch/scripts/create-piptline.ts
@@ -0,0 +1,76 @@
+import { Client } from "@elastic/elasticsearch";
+// import {client as esClient} from "../../../elasticsearch";
+import { AUTO_ID_TIMESTAMP_PIPELINE, AUTO_TIMESTAMP_PIPELINE } from "../../../../domains/search/constants";
+
+const createPipeLine = async (id: string, config: {}, client: Client) => {
+
+    await client.ingest.putPipeline({
+        id,
+        body: config
+    })
+};
+
+export const createIdWithTimestampPipeline = async (client: Client) => {
+    const config = {
+        description: "Creates a timestamp and id when a document is initially indexed",
+        processors: [
+            {
+                script: {
+                    source: `
+                        String getUUID(def str) {
+                            def res = null;
+                            if (str != null) {
+                            char[] buffer = str.toCharArray();
+                            byte[] b = new byte[buffer.length];
+                            for (int i = 0; i < b.length; i++) {
+                                b[i] = (byte) buffer[i];
+                            }
+                            res = UUID.nameUUIDFromBytes(b).toString();
+                            } else {
+                                // randomUUID does not work
+                                //res = UUID.randomUUID().toString();
+                                res = Math.random().toString();
+                            }
+                            return res;
+                        }
+                        
+                        //
+                        // Main
+                        // params.field - the field name.
+                        // Note: "doted" names (like "content.message") will not work.
+                        //
+                        if (ctx.containsKey(params.field) && !ctx[params.field].isEmpty()) {
+                            ctx._id = getUUID(ctx[params.field]) + "." + getUUID(ctx[params.field].encodeBase64());
+                        } else {
+                            ctx._id = params.field + "_empty_" + getUUID(null);
+                        }
+                    `
+                }
+            },
+            {
+                set: {
+                    field: "_source.timestamp",
+                    value: "{{_ingest.timestamp}}"
+                }
+            }
+        ]
+    };
+
+    await createPipeLine(AUTO_ID_TIMESTAMP_PIPELINE, config, client);
+}
+
+export const createTimestampPipeline = async (client: Client) => {
+    const config = {
+        description: "Creates a timestamp when a document is initially indexed",
+        processors: [
+            {
+                set: {
+                    field: "_source.timestamp",
+                    value: "{{_ingest.timestamp}}"
+                }
+            }
+        ]
+    };
+
+    await createPipeLine(AUTO_TIMESTAMP_PIPELINE, config, client);
+}
