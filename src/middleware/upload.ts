a552791a4752f88b8bc47d7a03533462038385d2
ehgks0000 <ehgks0083@gmail.com>
2023-07-31 22:17:17 +0900

    REFACTOR: multer s3 마이그레이션

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,9 +1,18 @@
 import multer from "multer";
+import { S3Client } from "@aws-sdk/client-s3";
+import multerS3 from "multer-s3";
 import path from "path";
 import { conf } from "../config";
 
-const { IMAGES_DIR_PATH, MAX_FILE_SIZE, MAX_FILES } = conf();
+const { MAX_FILE_SIZE, MAX_FILES, ACCESS_KEY_ID, SECRET_ACCESS_KEY } = conf();
 
+const s3 = new S3Client({
+    region: "ap-northeast-2",
+    credentials: {
+        accessKeyId: ACCESS_KEY_ID,
+        secretAccessKey: SECRET_ACCESS_KEY
+    }
+});
 /**
  *
  *
@@ -42,25 +51,23 @@ const { IMAGES_DIR_PATH, MAX_FILE_SIZE, MAX_FILES } = conf();
 
 export const upload = ({ prefix }: { prefix?: string } = {}) =>
     multer({
-        storage: multer.diskStorage({
-            destination: (req, file, cb) => {
-                cb(null, IMAGES_DIR_PATH);
-            },
-            filename: (req, file, cb) => {
+        storage: multerS3({
+            s3,
+            bucket: "dev-rc-1",
+            key(req, file, callback) {
                 const ext = path.extname(file.originalname); // 확장자 추출
 
                 if (!req.id) {
                     throw new Error("Check your auth");
                 }
-                /**
-                 * @format "https://wadada.me/10-d8-1684206706483.png"
-                 */
+
                 const fileNameT = file.originalname.split(".")[0];
                 let fileName = `${req.id}-${fileNameT.substring(0, 2)}-${Date.now()}${ext}`;
                 if (prefix) {
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
                     fileName = `${prefix}-${fileName}`;
                 }
-                cb(null, fileName);
+                callback(null, fileName);
             }
         }),
         fileFilter: (req, file, cb) => {
@@ -75,3 +82,38 @@ export const upload = ({ prefix }: { prefix?: string } = {}) =>
             files: MAX_FILES
         }
     });
+// export const upload = ({ prefix }: { prefix?: string } = {}) =>
+//     multer({
+//         storage: multer.diskStorage({
+//             destination: (req, file, cb) => {
+//                 cb(null, IMAGES_DIR_PATH);
+//             },
+//             filename: (req, file, cb) => {
+//                 const ext = path.extname(file.originalname); // 확장자 추출
+
+//                 if (!req.id) {
+//                     throw new Error("Check your auth");
+//                 }
+//                 /**
+//                  * @format "https://wadada.me/10-d8-1684206706483.png"
+//                  */
+//                 const fileNameT = file.originalname.split(".")[0];
+//                 let fileName = `${req.id}-${fileNameT.substring(0, 2)}-${Date.now()}${ext}`;
+//                 if (prefix) {
+//                     fileName = `${prefix}-${fileName}`;
+//                 }
+//                 cb(null, fileName);
+//             }
+//         }),
+//         fileFilter: (req, file, cb) => {
+//             if (file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg") {
+//                 return cb(null, true);
+//             }
+
+//             return cb(null, false);
+//         },
+//         limits: {
+//             fileSize: MAX_FILE_SIZE, // 600kb
+//             files: MAX_FILES
+//         }
+//     });
